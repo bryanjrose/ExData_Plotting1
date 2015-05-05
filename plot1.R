792464101d8da0d1d65e0ddd6c5b9b92d25f0e06
bryanjrose <brose9@gmail.com>
2015-05-05 16:25:30 -0400

    new file:   plot1.R 	new file:   plot1.png

---
+++ plot1.R
@@ -0,0 +1,58 @@
+#########################################
+# Exploratory Data Analysis Project 1
+# plot1.R
+# Coursera course: exdata-014
+# bryanjrose 05-May-15
+#########################################
+
+# Load libraries if necessary
+if(!require(ggplot2)){install.packages("ggplot2")}
+library(ggplot2)
+
+if(!require(lubridate)){install.packages("lubridate")}
+library(lubridate)
+
+# Check for the text file in working directory, download if not present
+# Place in temporary directory to unzip.  Remove temp directory when done.
+linkaddress <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+tempzip <- "tempdata/ElecData.zip"
+datafile <- "household_power_consumption.txt"
+
+if (!file.exists(datafile)) {
+        if (!file.exists("tempdata")) {dir.create("tempdata")}
+        download.file(linkaddress, destfile=tempzip) # for Windows OS
+        unzip(tempzip)
+        unlink("tempdata", recursive=TRUE)
+}
+
+#Quickly get headers and classes of each column
+tablheads <- read.table(datafile, header = TRUE, sep = ";", nrows = 5)
+tablclass <- sapply(tablheads, class)
+
+#Import just the rows needed for this project (dates = 1/2/2007 to 2/2/2007)
+#Manually adjust line numbers if necessary
+linestart <- 66638
+lineend <- 69517
+numlines <- lineend - linestart
+
+#Reads the appropriate text file rows and use the same column names found above
+#Using the colClasses found above makes reading faster
+tablRaw <- read.table(datafile, header = FALSE, sep = ";", col.names = names(tablheads),
+                      na.strings = "?", colClasses = tablclass, 
+                      skip = linestart - 1, nrows = numlines + 1)
+
+#Combine the first two columns 
+#get rid of extra time column and class the date column
+tablRaw$Date <- paste(tablRaw$Date, tablRaw$Time)
+data <- subset(tablRaw, select = -Time)
+data$Date <- dmy_hms(data$Date)
+
+#Create plot1 - histogram with red bars and customized labels
+png("plot1.png", width = 480, height = 480, units = "px")
+hist(data$Global_active_power, xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power", col = "red")
+dev.off()
+
+
+
+
